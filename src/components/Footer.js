bcac26c45e5c8ca4220270be9f6005834054d98f
Ahumuza <lizahumuza@gmail.com>
2021-10-25 18:55:45 +0300

    modified footer font

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,16 @@ import React from 'react'
 import {Box, HStack, Text} from '@chakra-ui/react'
 import Socialicons from './Socialicons';
 import Footlinks from './Footlinks';
+import '../index.css'
 
 const Footer = () => {
     return (  
         <Box w="100%">
             <Footlinks/>
         <HStack marginTop="40px" spacing="300px" h="100%" marginLeft="40px" w="100%">
-            <Text color="#FFFFFF" fontSize="30px" fontWeight="700" display={{ base: "none", md: "block" }}>TicketDEX</Text>
+            <Text color="#FFFFFF" fontSize="30px" fontWeight="700" fontFamily="Poppins" display={{ base: "none", md: "block" }}>TicketDEX</Text>
             <Socialicons />
-            <Text color="#FFFFFF" fontSize="14px" fontFamily="Poppins" fontWeight="500"> ©2021 TicketDex.All rights reserved.</Text>
+            <Text color="#FFFFFF" fontSize="14px" fontFamily="Poppins" fontWeight="550"> ©2021 TicketDex.All rights reserved.</Text>
         </HStack>
         </Box>
     );
--- src/components/Footlinks.js
+++ src/components/Footlinks.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link, Flex} from '@chakra-ui/react'
+import '../index.css'
 
 const Footlinks = () => {
     return ( 
         <Flex direction="row" marginTop="40px" marginLeft="30px" marginRight="30px" height="100%" justifyContent="space-around">
-            <Link href="#" textDecoration="underline" fontSize= "0.8rem" color="#FFFFFF" fontFamily="Poppins" fontWeight="300" lineHeight= "1.5rem" >WALLETS</Link>
+            <Link href="#" textDecoration="underline" fontSize= "0.8rem" color="#FFFFFF" fontFamily="Poppins" fontWeight="500" lineHeight= "1.5rem" >WALLETS</Link>
             
-            <Link href="#" textDecoration="underline" fontSize="0.8rem" color="#FFFFFF" fontFamily="Poppins" fontWeight="300" lineHeight= "1.5rem" >FAQ</Link>
+            <Link href="#" textDecoration="underline" fontSize="0.8rem" color="#FFFFFF" fontFamily="Poppins" fontWeight="500" lineHeight= "1.5rem" >FAQ</Link>
             
-            <Link href="#" textDecoration="underline" fontSize="0.8rem" color="#FFFFFF" fontFamily="Poppins" fontWeight="300" lineHeight= "1.5rem" >HELP</Link>
+            <Link href="#" textDecoration="underline" fontSize="0.8rem" color="#FFFFFF" fontFamily="Poppins" fontWeight="500" lineHeight= "1.5rem" >HELP</Link>
         </Flex>
         
          
