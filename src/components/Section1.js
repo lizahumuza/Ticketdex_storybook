ea0a442c1a64da523286a88f312487518c51682b
Ahumuza <lizahumuza@gmail.com>
2021-10-25 21:12:42 +0300

    added a section of the body

---
+++ src/components/Section1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Heading, Box, Text } from '@chakra-ui/react';
+import '../index.css'
+
+const Section1 = () => {
+    return (  
+        <Box>
+            <Heading color="#FFFFFF" fontSize="4.5rem" fontFamily="Poppins" fontWeight="700" textAlign="center">TICKETING <br></br>JUST GOT PROFITABLE</Heading>
+            <Text textAlign="center" color="#888888" fontSize="1.5em" fontFamily="Poppins">The fastest and safest platform to buy and sell tickets.</Text>
+        </Box>
+    );
+}
+ 
+export default Section1;
